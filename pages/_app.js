d04cfbb2ec16c86a95e559113fe8b6e0c8cd7a02
Shu Ding <g@shud.in>
2021-01-08 00:16:14 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
 
+import '../styles/main.css'
+
 export default function Nextra({ Component, pageProps }) {
   return <>
     <Head>
@@ -9,6 +11,13 @@ export default function Nextra({ Component, pageProps }) {
         type="application/rss+xml"
         title="RSS"
         href="/feed.xml"
+      />
+      <link
+        rel="preload"
+        href="/fonts/Inter-roman.latin.var.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
       />
     </Head>
     <Component {...pageProps} />
